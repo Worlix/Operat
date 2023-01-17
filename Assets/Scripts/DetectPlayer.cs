66d3d5cdd9cad8d69c71786acd5a6aea08979d26
Worlix <vasco_cunn@hotmail.com>
2023-01-17 16:16:58 +0100

    script detectplayer

---
+++ Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    public bool JoueurEstProche;
+    public GameObject kbIconInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find(kbIconInfo.name).GetComponent<SpriteRenderer>().enabled = false;
+        JoueurEstProche = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && JoueurEstProche)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player potentialPlayer = collision.gameObject.GetComponent<player>();
+        if (potentialPlayer == null)
+            return;
+        GameObject.Find(kbIconInfo.name).GetComponent<SpriteRenderer>().enabled = true;
+        JoueurEstProche = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameObject.Find(kbIconInfo.name).GetComponent<SpriteRenderer>().enabled = false;
+        JoueurEstProche = false;
+    }
+}
