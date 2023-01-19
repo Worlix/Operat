0bde0f4507ce11b9c0790b313edefcae7d6f5d93
Worlix <vasco_cunn@hotmail.com>
2023-01-19 15:46:30 +0100

    pointeur marche un peu

--- Assets/Scripts/TaskPointer.cs
+++ Assets/Scripts/TaskPointer.cs
@@ -5,12 +5,16 @@ using CodeMonkey.Utils;
 
 public class TaskPointer : MonoBehaviour
 {
-    public Vector3 targetPosition;
+    private Vector3 targetPosition;
     private RectTransform pointerRect;
+    public GameObject target;
+    [SerializeField] private Camera uiCamera;
+    public float borderSize = 1f;
 
     private void Awake()
     {
         pointerRect = transform.Find("Pointer").GetComponent<RectTransform>();
+        targetPosition = target.transform.position;
     }
 
     // Update is called once per frame
@@ -19,8 +23,30 @@ public class TaskPointer : MonoBehaviour
         Vector3 toPosition = targetPosition;
         Vector3 fromPosition = Camera.main.transform.position;
         fromPosition.z = 0f;
-        Vector3 dir = toPosition - fromPosition.normalized;
+        Vector3 dir = toPosition - fromPosition;
         float angle = UtilsClass.GetAngleFromVectorFloat(dir);
         pointerRect.localEulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
+        bool isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
+
+        if (isOffScreen)
+        {
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            if (cappedTargetScreenPosition.x <= borderSize) cappedTargetScreenPosition.x = borderSize;
+            if (cappedTargetScreenPosition.x >= Screen.width - borderSize) cappedTargetScreenPosition.x = Screen.width - borderSize;
+            if (cappedTargetScreenPosition.y <= borderSize) cappedTargetScreenPosition.y = borderSize;
+            if (cappedTargetScreenPosition.y >= Screen.height - borderSize) cappedTargetScreenPosition.y = Screen.height - borderSize;
+
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPosition);
+            pointerRect.position = pointerWorldPosition;
+            pointerRect.localPosition = new Vector3(pointerRect.localPosition.x, pointerRect.localPosition.y, 0f);
+        }
+        else
+        {
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(targetPositionScreenPoint);
+            pointerRect.position = pointerWorldPosition;
+            pointerRect.localPosition = new Vector3(pointerRect.localPosition.x, pointerRect.localPosition.y, 0f);
+        }
     }
 }
